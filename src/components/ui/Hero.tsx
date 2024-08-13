23d1b187f7b283f7049d9596a182a57f3f5c3c04
Ankan <ankanganguly24@gmail.com>
2024-08-13 14:51:13 +0530

    added button on hero to add the view book routing

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -1,20 +1,24 @@
 import Image from "next/image";
 import React from "react";
 import book from "../../../public/book.png";
+import { Button } from "./button";
 
 const Hero = () => {
   return (
-    <div className="flex justify-between px-10 items-center mb-10 border-b">
-      <div className="w-[50%]">
-        <h1 className="text-4xl font-bold text-primary mb-3">Book Wishlist</h1>
-        <p className="text-sm">
+    <div className="flex justify-between md:px-10 px-2 py-3 md:py-0 items-center mb-10 border-b">
+      <div className="md:w-[50%] w-full">
+        <h1 className="text-4xl font-bold text-primary">Book Wishlist</h1>
+        <p className="text-sm my-3">
           Buy your favorite books to your wishlist with us. Discover new reads,
           track your literary desires, and keep all your must-haves in one
           place. Start building your dream library today!
         </p>
+        <div>
+          <Button variant={"outline"}> See your wishlist! </Button>
+        </div>
       </div>
 
-      <div>
+      <div className="hidden md:block">
         <Image src={book} width={400} height={400} alt="Book Image" />
       </div>
     </div>
